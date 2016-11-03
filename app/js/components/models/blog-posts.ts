78469ac26e713ae22c8f677c222165eda4c74dc8
Scott Marlatt <scottmarlatt@Scotts-MacBook-Pro.local>
2016-11-02 22:21:06 -0700

    testing image serving

--- app/js/components/models/blog-posts.ts
+++ app/js/components/models/blog-posts.ts
@@ -4,7 +4,7 @@ export const BLOGPOSTS: BlogPost[] = [
 	{
 		id: 0, 
 	    title: 'winding down', 
-    	img:'http://127.0.0.1:8080/images/blog/castro-sunset.jpg', 
+    	img:'../../../../public/images/blog/castro-sunset.jpg', 
 	    body: 'what a summer it was.\n\nthere\'s something about summer that really makes it stand out. you can\'t quite put your finger on it but it\'s there. since moving out here i\'ve been blessed with great weather year round...'
 	}
 ];

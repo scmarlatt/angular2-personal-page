b63a24b5c07cd98d22744eae2b3ebfd4ab0f001f
Scott Marlatt <scottmarlatt@Scotts-MacBook-Pro.local>
2016-11-10 22:19:02 -0800

    oops

--- app/js/components/models/blog-posts.ts
+++ app/js/components/models/blog-posts.ts
@@ -6,6 +6,6 @@ export const BLOGPOSTS: BlogPost[] = [
 	    title: 'winding down', 
 	    date: 'Oct. 29, 2016 :: 9:09PM',
     	img:'https://s3-us-west-1.amazonaws.com/scm-personal-page/personal-page/images/blog/castro-sunset.jpg', 
-    	html: `<h2>Winding Down</h2><br><h3>{{selectedBlogPost.date}}</h3><br><p><b>what a summer it was</b></p><p>there\'s something about summer that really makes it stand out. you can\'t quite put your finger on it but it\'s there. since moving out here i\'ve been blessed with great weather year round...</p>`
+    	html: `<h2>Winding Down</h2><br><h3>{{selectedBlogPost.date}}</h3><br><p><b>what a summer it was</b></p><p>sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample sample </p>`
 	}
 ];
